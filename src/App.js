984d9e2296148b88020a425f27dabb3c5d7e7d04
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2021-11-19 09:25:21 +0700

    change into another class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Halo, Selamat siang saat ini kita sedang belajar CI/CD TI-3D
-        </p>
+        <p>Halo, Selamat siang saat ini kita sedang belajar CI/CD TI-3H</p>
         <a
           className="App-link"
           href="https://reactjs.org"
